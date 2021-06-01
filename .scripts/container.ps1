d8fe54900601d81fc472404f89df0a8b01d15b1f
Adam Szabo <adam.szabo@cosmoconsult.com>
2021-06-01 19:15:52 +0200

    container creator ps script added

---
+++ .scripts/container.ps1
@@ -0,0 +1,21 @@
+﻿<#
+ # Dependencies
+   - Docker
+   - BcContainerHelper
+       https://www.powershellgallery.com/packages/BcContainerHelper/
+#>
+
+$creds = New-Object System.Management.Automation.PSCredential -argumentList "admin", (ConvertTo-SecureString -String "Passw0rd" -AsPlainText -Force)
+$containerName = "bc-tst"
+
+New-BcContainer -accept_eula `
+                -containerName $containerName `
+                -artifactUrl (Get-BCArtifactUrl -type OnPrem -country w1) `
+                -auth NavUserPassword `
+                -Credential $creds `
+                -useSSL `
+                -updateHosts `
+                -installCertificateOnHost `
+                -dns 8.8.8.8
+
+Import-TestToolkitToBcContainer -containerName $containerName -includeTestLibrariesOnly
